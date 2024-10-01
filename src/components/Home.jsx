021a596a58ec22349ad938c55c613f690ce04a1d
Fabrice <f.nkurunziz@alustudent.com>
2024-10-01 21:28:37 +0200

    add the side bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,41 @@ import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
 import Person3OutlinedIcon from '@mui/icons-material/Person3Outlined';
 import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
 import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 import styles from './Home.module.css';
 
 function Home() {
 	return (
 		<div className={styles.mainContainer}>
+			<div className={styles.sideBar}>
+				<ul className={styles.sideBarCategories}>
+					<li className={styles.category}>
+						<WindowIcon />
+						<span className={styles.categoryName}>
+							Home
+						</span>
+					</li>
+					<li className={styles.category}>
+						<CreditCardOutlinedIcon />
+						<span className={styles.categoryName}>
+							Payment
+						</span>
+					</li>
+					<li className={styles.category}>
+						<CreditCardOutlinedIcon />
+						<span className={styles.categoryName}>
+							Payment
+						</span>
+					</li>
+					<li className={styles.category}>
+						<CreditCardOutlinedIcon />
+						<span className={styles.categoryName}>
+							Payment
+						</span>
+					</li>
+				</ul>
+			</div>
 			<div className={styles.navigation}>
 				<ul className={styles.categories}>
 					<li className={styles.category}>
@@ -52,6 +81,9 @@ function Home() {
 					</li>
 					<li className={styles.setting}>
 						<WbSunnyOutlinedIcon />
+					</li>
+					<li className={styles.setting}>
+						<LogoutOutlinedIcon />
 					</li>
 				</ul>
 			</div>
